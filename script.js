a9099c37d739dc9e4fcdce34334166cbc0c90275
i3z1 <azozsony1@gmail.com>
2023-10-27 04:00:26 +0300

    First commit

--- script.js
+++ script.js
@@ -5,69 +5,78 @@ document.addEventListener("DOMContentLoaded", function() {
     canvas.height = window.innerHeight;
     document.getElementById("constellations").appendChild(canvas);
 
-    const stars = [];
-
-    for (let i = 0; i < 500; i++) {
-      stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 3 + 1,
-        speedX: (Math.random() - 0.5) * 1,
-        speedY: (Math.random() - 0.5) * 1
-      });
-    }
-
-    let mouse = {
-        x: undefined,
-        y: undefined
-    };
-
-    window.addEventListener('mousemove', function(event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     });
 
+    const stars = Array.from({ length: 500 }, () => ({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 2 + 0.5,
+        speedX: (Math.random() - 0.5),
+        speedY: (Math.random() - 0.5),
+        color: `rgba(${255}, ${255}, ${255}, ${Math.random()})` // random opacity
+    }));
+
+    const mouse = { x: undefined, y: undefined };
+
+    window.addEventListener('mousemove', e => {
+        mouse.x = e.x;
+        mouse.y = e.y;
+    });
+
+
+
+
+
     function draw() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      ctx.fillStyle = "#ffffff";
-      stars.forEach(star => {
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-        ctx.fill();
-        
-        let dx = star.x - mouse.x;
-        let dy = star.y - mouse.y;
-        let distance = Math.sqrt(dx * dx + dy * dy);
+        ctx.fillStyle = "#ffffff";
+        stars.forEach(star => {
+            ctx.beginPath();
+            ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+            ctx.fill();
 
-        if (distance < 100) {
-            let angle = Math.atan2(dy, dx);
-            star.x += Math.cos(angle) * 3;
-            star.y += Math.sin(angle) * 3;
-        } else {
-            star.x += star.speedX;
-            star.y += star.speedY;
-        }
+            const dx = star.x - mouse.x;
+            const dy = star.y - mouse.y;
+            const distance = Math.sqrt(dx ** 2 + dy ** 2);
 
-        if (star.x < 0 || star.x > canvas.width) star.speedX = -star.speedX;
-        if (star.y < 0 || star.y > canvas.height) star.speedY = -star.speedY;
-      });
+            if (distance < 100) {
+                const angle = Math.atan2(dy, dx);
+                star.x += Math.cos(angle) * 3;
+                star.y += Math.sin(angle) * 3;
+            } else {
+                star.x += star.speedX;
+                star.y += star.speedY;
+            }
 
-      for (let i = 0; i < stars.length; i++) {
-        const star1 = stars[i];
-        ctx.beginPath();
-        ctx.moveTo(star1.x, star1.y);
-        for (let j = i + 1; j < stars.length; j++) {
-          const star2 = stars[j];
-          if (Math.abs(star1.x - star2.x) < 100 && Math.abs(star1.y - star2.y) < 100) {
-            ctx.lineTo(star2.x, star2.y);
-          }
-        }
-        ctx.strokeStyle = "#808080";
-        ctx.stroke();
-      }
+            if (star.x < 0 || star.x > canvas.width) star.speedX = -star.speedX;
+            if (star.y < 0 || star.y > canvas.height) star.speedY = -star.speedY;
+        });
 
-      requestAnimationFrame(draw);
+        ctx.strokeStyle = "#35333f";
+        ctx.lineWidth = 0.2;
+
+        stars.forEach((star1, i) => {
+            ctx.beginPath();
+            ctx.moveTo(star1.x, star1.y);
+
+            stars.slice(i + 1).forEach(star2 => {
+                const dx = star1.x - star2.x;
+                const dy = star1.y - star2.y;
+                const distance = Math.sqrt(dx ** 2 + dy ** 2);
+
+                if (distance < 100) {  // Reduced this distance to make smaller constellations
+                    ctx.lineTo(star2.x, star2.y);
+                }
+            });
+
+            ctx.stroke();
+        });
+
+        requestAnimationFrame(draw);
     }
 
     draw();
